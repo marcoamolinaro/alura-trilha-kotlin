f8b378e5f076e8e7c4f5d5025d473ec3ef285e4d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-05-14 14:27:29 -0300

    First commit

---
+++ primeira-aplicacao/AluGames/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package org.example
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    println("Hello World, Kotlin!")
+    println("Program arguments: ${args.joinToString()}")
+}
