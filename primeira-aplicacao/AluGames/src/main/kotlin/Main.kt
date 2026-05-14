d8505adab14c70560c93c11f58212ae68602f25f
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-05-14 15:05:39 -0300

    Getting endpoint for example

--- primeira-aplicacao/AluGames/src/main/kotlin/Main.kt
+++ primeira-aplicacao/AluGames/src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(args: Array<String>) {
-    println("Hello World, Kotlin!")
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val client: HttpClient? = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+    val response: HttpResponse<String?>? = client!!
+        .send(request, BodyHandlers.ofString())
+
+    val json = response?.body()
+
+    println(json)
 }
