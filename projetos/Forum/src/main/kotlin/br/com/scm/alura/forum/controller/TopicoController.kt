add594a50f045db72645e26fb8720596198c4cdc
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-05-29 17:57:04 -0300

    Adding post topicos endpoint

--- projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/controller/TopicoController.kt
+++ projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/controller/TopicoController.kt
@@ -4,6 +4,8 @@ import br.com.scm.alura.forum.model.Topico
 import br.com.scm.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,5 +23,10 @@ class TopicoController(
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long) : Topico? {
         return topicoService.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico) {
+        topicoService.cadastrar(topico)
     }
 }
--- projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/service/TopicoService.kt
+++ projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/service/TopicoService.kt
@@ -1,66 +1,11 @@
 package br.com.scm.alura.forum.service
 
-import br.com.scm.alura.forum.model.Curso
 import br.com.scm.alura.forum.model.Topico
-import br.com.scm.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 
 @Service
 class TopicoService(
-    private var topicos: List<Topico>
-) {
-
-    init {
-        val topico1 = Topico(
-            1,
-            titulo = "Duvida sobre Spring Boot",
-            mensagem = "Alguem pode me ajudar com Spring Boot?",
-            curso = Curso(
-                1,
-                nome = "Duvida sobre Spring Boot",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                1,
-                nome = "Marco",
-                email = "marco@email.com"
-            )
-        )
-
-        val topico2 = Topico(
-            2,
-            titulo = "Duvida sobre controller",
-            mensagem = "Alguem pode me ajudar com controller?",
-            curso = Curso(
-                1,
-                nome = "Duvida sobre Spring Boot",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                2,
-                nome = "Carmen",
-                email = "carmen@email.com"
-            )
-        )
-
-        val topico3 = Topico(
-            3,
-            titulo = "Duvida sobre Kotlin",
-            mensagem = "Alguem pode me ajudar com construtores?",
-            curso = Curso(
-                2,
-                nome = "Duvida sobre Kotlin",
-                categoria = "Programação",
-            ),
-            autor = Usuario(
-                3,
-                nome = "Sofia",
-                email = "sofia@email.com"
-            )
-        )
-
-        topicos = listOf(topico1, topico2, topico3)
-    }
+    private var topicos: List<Topico> = ArrayList()) {
 
     fun listar() : List<Topico> {
         return topicos
@@ -68,5 +13,9 @@ class TopicoService(
 
     fun buscarPorId(id: Long) : Topico? {
         return topicos.find { it.id == id }
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos = topicos.plus(topico)
     }
 }
