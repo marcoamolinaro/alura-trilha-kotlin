35e7c83ff30320e8cdf14aa7a9478247c5e9ccb3
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-05-29 15:13:48 -0300

    Adding model classes

---
+++ projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/model/Curso.kt
@@ -0,0 +1,7 @@
+package br.com.scm.alura.forum.model
+
+data class Curso(
+    val id: Long? = null,
+    val nome: String,
+    val categoria: String
+)
---
+++ projetos/Forum/src/main/kotlin/br/com/scm/alura/forum/model/Usuario.kt
@@ -0,0 +1,7 @@
+package br.com.scm.alura.forum.model
+
+data class Usuario (
+    val id: Long? = null,
+    val nome: String,
+    val email: String
+)
